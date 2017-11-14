1228a2a17431b173d842054bc2a783c3cd62e464
RuiDTLima <rui_lima24@hotmail.com>
2017-11-14 12:05:51 +0000

    Finish exercise 1 in java two more tests to go

---
+++ 2º Trabalho/java/src/ConcurrentQueue.java
@@ -0,0 +1,76 @@
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ConcurrentQueue<T> {
+    private class Node<T> {
+        private T value;
+        private AtomicReference<Node<T>> next;
+
+        public Node(T value) {
+            this.value = value;
+            this.next = new AtomicReference<>(null);
+        }
+    }
+
+    private Node<T> dummy = new Node<>(null); // qual utilidade de dummy
+    private AtomicReference<Node<T>> head = new AtomicReference<>(dummy);
+    private AtomicReference<Node<T>> tail = new AtomicReference<>(dummy);
+
+    /**
+     * coloca no fim da fila o elemento passado como argumento
+     * @param elem
+     */
+    public void put(T elem){
+        Node<T> node = new Node<>(elem);
+
+        while (true) {
+            Node<T> observedNode = tail.get();
+            Node<T> observedNodeNext = observedNode.next.get();
+
+            if(observedNode == tail.get()) {
+                if (observedNodeNext != null) {  // significa que nesta iteração do while o o valor apontado pelo tail já tem next
+                    tail.compareAndSet(observedNode, observedNodeNext);
+                } else {
+                    if (observedNode.next.compareAndSet(null, node)) {
+                        tail.compareAndSet(observedNode, node);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * retorna o elemento presente no início da fila ou null , no caso da fila estar vazia
+     * @return
+     */
+    public T tryTake(){
+        while (true) {//check tail
+            if (isEmpty())
+                return null;
+            Node<T> observedHead = head.get();
+            Node<T> node = observedHead.next.get();
+            if(node != null)
+                if(head.compareAndSet(observedHead, node)) {
+                    T value = node.value;
+                    node.value = null;
+                    return value;
+                }
+        }
+    }
+
+    /**
+     * indica se a fila está vazia
+     * @return
+     */
+    public boolean isEmpty(){
+        return head.get().next.get() == null;
+    }
+
+    public T dequeue() throws InterruptedException {
+        T v;
+        while ((v = tryTake()) == null) {
+            Thread.sleep(0);
+        }
+        return v;
+    }
+}
---
+++ 2º Trabalho/java/test/TestConcurrentQueue.java
@@ -0,0 +1,121 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestConcurrentQueue {
+    @Test
+    public void testOneElementPutAndTryTake(){
+        ConcurrentQueue<String> queue = new ConcurrentQueue<>();
+        String messageSend = "Hello World";
+
+        Assert.assertTrue(queue.isEmpty());
+        queue.put(messageSend);
+
+        Assert.assertFalse(queue.isEmpty());
+        String result = queue.tryTake();
+        Assert.assertTrue(queue.isEmpty());
+
+        Assert.assertEquals(messageSend, result);
+    }
+
+    @Test
+    public void TestTwoElementPutAndTryTake(){
+        ConcurrentQueue<String> queue = new ConcurrentQueue<>();
+        String firstMessageSend = "Hello World";
+        String secondMessageSend = "ISEL";
+
+        Assert.assertTrue(queue.isEmpty());
+        queue.put(firstMessageSend);
+
+        Assert.assertFalse(queue.isEmpty());
+        queue.put(secondMessageSend);
+
+        String firstResult = queue.tryTake();
+        Assert.assertFalse(queue.isEmpty());
+
+        String secondResult = queue.tryTake();
+
+        Assert.assertTrue(queue.isEmpty());
+
+        Assert.assertEquals(firstMessageSend, firstResult);
+        Assert.assertEquals(secondMessageSend, secondResult);
+    }
+
+    @Test
+    public void TestMultiplePutAndTryTake(){
+        ConcurrentQueue<String> queue = new ConcurrentQueue<>();
+        String[] messages = {"Hello World", "Hello ISEL", "This is Threads", "This is PC", "Windows"};
+        String[] results = new String[messages.length];
+        Assert.assertTrue(queue.isEmpty());
+
+        for (int i = 0; i < messages.length; i++) {
+            queue.put(messages[i]);
+        }
+
+        Assert.assertFalse(queue.isEmpty());
+
+        for (int i = 0; i < messages.length; i++) {
+            results[i] = queue.tryTake();
+        }
+
+        Assert.assertTrue(queue.isEmpty());
+
+        for (int i = 0; i < messages.length; i++) {
+            Assert.assertEquals(messages[i], results[i]);
+        }
+    }
+
+    @Test
+    public void testTryTakeNullReturn(){
+        ConcurrentQueue<String> queue = new ConcurrentQueue<>();
+
+        Assert.assertNull(queue.tryTake());
+    }
+
+    @Test
+    public void testMultipleThreadsPutAndTryTake() throws InterruptedException {
+        ConcurrentQueue<String> queue = new ConcurrentQueue<>();
+
+        Assert.assertTrue(queue.isEmpty());
+
+        String[] messages = {"Hello World", "Hello ISEL", "This is Threads", "This is PC", "Windows"};
+        String[] results = new String[messages.length];
+        Thread[] threads = new Thread[messages.length];
+
+        for (int i = 0; i < threads.length; i++) {
+            int j = i;
+            threads[j] = new Thread(()-> queue.put(messages[j]));
+            threads[i].start();
+            Thread.sleep(10);
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+        }
+
+        Assert.assertFalse(queue.isEmpty());
+
+        for (int i = 0; i < threads.length; i++) {
+            int j = i;
+            threads[j] = new Thread(()-> results[j] = queue.tryTake());
+            threads[i].start();
+            Thread.sleep(10);
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].join();
+            Assert.assertEquals(messages[i], results[i]);
+        }
+
+        Assert.assertTrue(queue.isEmpty());
+    }
+
+    @Test
+    public void testInterruptedPut(){
+
+    }
+
+    @Test
+    public void testInterruptesTryTake(){
+
+    }
+}
